911e23365843893db4a474cc35acd053b99d2353
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2023-04-15 22:58:34 +0200

    🚧 Initial `Triangle` implementation

--- src/surface.rs
+++ src/surface.rs
@@ -1,5 +1,6 @@
 mod fog;
 mod sphere;
+mod triangle;
 
 use std::ops::Range;
 
@@ -12,19 +13,22 @@ use crate::math::hit::*;
 use crate::math::ray::Ray;
 use crate::surface::fog::UniformFog;
 use crate::surface::sphere::Sphere;
+use crate::surface::triangle::Triangle;
 
 /// Surface that is being rendered.
 #[derive(Deserialize, JsonSchema)]
 #[serde(tag = "type")]
 pub enum Surface {
     Sphere(Sphere),
+    Triangle(Triangle),
     UniformFog(UniformFog),
 }
 
-impl const Bounded for Surface {
+impl Bounded for Surface {
     fn aabb(&self) -> Aabb {
         match self {
             Self::Sphere(sphere) => sphere.aabb(),
+            Self::Triangle(triangle) => triangle.aabb(),
             Self::UniformFog(fog) => fog.aabb,
         }
     }
@@ -34,6 +38,7 @@ impl Hittable for Surface {
     fn hit(&self, by_ray: &Ray, distance: &Range<f64>, rng: &Rng) -> Option<Hit> {
         match self {
             Self::Sphere(sphere) => sphere.hit(by_ray, distance, rng),
+            Self::Triangle(triangle) => triangle.hit(by_ray, distance, rng),
             Self::UniformFog(fog) => fog.hit(by_ray, distance, rng),
         }
     }
